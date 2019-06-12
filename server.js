7a0969ea8a70dbdc62fce04edb981bc5a8643247
Yann Moussaba <moussabayann@gmail.com>
2019-06-12 16:29:55 +0000

    Initial commit

---
+++ server.js
@@ -0,0 +1,18 @@
+'use strict';
+
+const express = require('express');
+const request = require("request");
+
+// Constants
+const PORT = 8080;
+const HOST = '0.0.0.0';
+
+// App
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send("Hello world")
+});
+
+app.listen(PORT, HOST);
+console.log(`Running on http://${HOST}:${PORT}`);
