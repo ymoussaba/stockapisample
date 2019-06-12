2b41df6d58568f6507f765c80f7bb441b42f6d74
Yann Moussaba <moussabayann@gmail.com>
2019-06-12 16:30:33 +0000

    Return real time stock ticker from external api

--- server.js
+++ server.js
@@ -10,8 +10,62 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 
+// Stock exchange symbols
+const symbols = [
+    "GOOG",
+    "AAPL",
+    "FB",
+    "AMZN",
+    "MSFT",
+];
+
 app.get('/', (req, res) => {
-    res.send("Hello world")
+    var series = [];
+
+    for (const symbol of symbols) {
+        // get stock time series
+        const options = {
+            method: 'GET',
+            url: 'https://www.alphavantage.co/query',
+            qs:
+            {
+                function: 'TIME_SERIES_INTRADAY',
+                symbol: symbol,
+                interval: '30min',
+                apikey: 'I9FF5A4FKWJZZN1Q'
+            }
+        };
+
+        request(options, function (error, response, body) {
+            if (error) throw new Error(error);
+            
+            // console.log("body : ", body);
+            const data = JSON.parse(Object(body));
+            // console.log("data : ", data);
+            const meta = data["Meta Data"];
+            // console.log("meta : ", meta);
+            var timeseries = Object.values(data)[1];
+            timeseries = Object.values(timeseries)[0];
+            // console.log("timeseries : ", timeseries);
+            const symbolSeries = {
+                symbol: meta["2. Symbol"],
+                open: timeseries["1. open"],
+                high: timeseries["2. high"],
+                low: timeseries["3. low"],
+                close: timeseries["4. close"],
+                volume: timeseries["5. volume"],
+            };
+
+            //const serie = Object.values(data)[1];
+            console.log(symbolSeries);
+            series.push(symbolSeries);
+
+            // send response when all symbol are retrieved
+            if(series.length == symbols.length) {
+                res.send(series);
+            }
+        });
+    }
 });
 
 app.listen(PORT, HOST);
