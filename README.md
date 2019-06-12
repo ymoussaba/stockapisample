829331731087054baafc7cba28e94ca0a7f34e71
Yann Moussaba <moussabayann@gmail.com>
2019-06-12 17:07:12 +0000

    Handled api error

---
+++ README.md
@@ -0,0 +1,3 @@
+# Stock API Sample
+
+Simple stock ticker prices API using Docker, Node JS, Express [Alpha Vantage](https://www.alphavantage.co/) API
--- server.js
+++ server.js
@@ -38,31 +38,45 @@ app.get('/', (req, res) => {
 
         request(options, function (error, response, body) {
             if (error) throw new Error(error);
-            
-            // console.log("body : ", body);
-            const data = JSON.parse(Object(body));
-            // console.log("data : ", data);
-            const meta = data["Meta Data"];
-            // console.log("meta : ", meta);
-            var timeseries = Object.values(data)[1];
-            timeseries = Object.values(timeseries)[0];
-            // console.log("timeseries : ", timeseries);
-            const symbolSeries = {
-                symbol: meta["2. Symbol"],
-                open: timeseries["1. open"],
-                high: timeseries["2. high"],
-                low: timeseries["3. low"],
-                close: timeseries["4. close"],
-                volume: timeseries["5. volume"],
-            };
 
-            //const serie = Object.values(data)[1];
-            console.log(symbolSeries);
-            series.push(symbolSeries);
+            if (response.statusCode == 200) {
+                // console.log("body : ", body);
+                const data = JSON.parse(Object(body));
 
-            // send response when all symbol are retrieved
-            if(series.length == symbols.length) {
-                res.send(series);
+                // handle api limit error
+                if(data.hasOwnProperty('Note')){
+                    res.send({
+                        error: "Sorry, we reached Alpha Vantage API Call limit (5 calls per minute and 500 calls per day), please retry again in a minute or contact support"
+                    });
+                } else {
+                    // console.log("data : ", data);
+                    const meta = data["Meta Data"];
+                    // console.log("meta : ", meta);
+                    var timeseries = Object.values(data)[1];
+                    timeseries = Object.values(timeseries)[0];
+                    // console.log("timeseries : ", timeseries);
+                    const symbolSeries = {
+                        symbol: meta["2. Symbol"],
+                        open: timeseries["1. open"],
+                        high: timeseries["2. high"],
+                        low: timeseries["3. low"],
+                        close: timeseries["4. close"],
+                        volume: timeseries["5. volume"],
+                    };
+    
+                    //const serie = Object.values(data)[1];
+                    // console.log(symbolSeries);
+                    series.push(symbolSeries);
+    
+                    // send response when all symbol are retrieved
+                    if (series.length == symbols.length) {
+                        res.send(series);
+                    }
+                }
+            } else {
+                res.send({
+                    error: "Unhandled error, please retry again or contact support"
+                });
             }
         });
     }
